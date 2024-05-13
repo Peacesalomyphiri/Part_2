83ff450a5515a68b5c5b94d9ae2a7de355d7dab9
Peace Salomy Phiri <st10163788@imconnect.edu.za>
2024-05-13 23:31:02 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Prog_Recipe_App_Part2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

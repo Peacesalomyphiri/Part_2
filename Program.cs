ed74fcc2e1780bdc073a8339c35b52520dd28a06
Peace Salomy Phiri <st10163788@imconnect.edu.za>
2024-05-15 02:53:29 +0200

    Added More Options To The Menu

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace Prog_Recipe_App_Part2
         public string FoodGroup { get; set; }
           
         public Ingredient(string name, double quantity, string unit, int calories, string foodGroup)
-        {
+        { 
             Name = name; 
             Quantity = quantity;
             Unit = unit;
